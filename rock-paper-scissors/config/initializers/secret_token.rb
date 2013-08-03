3ca5ca1d8db08381302db2306f888c20eea99c0c
jdlehman <jonathan.lehman91@gmail.com>
2013-08-03 01:10:07 -0400

    Initial commit

---
+++ rock-paper-scissors/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RockPaperScissors::Application.config.secret_token = 'c824c3f51fa5ec65ebf45b589734aec5b0edc052ec4d985ac1d096764b0acff9590bae25a4e407d26f4e751e626897b92de74068a54d717b7afd066992e999a1'
