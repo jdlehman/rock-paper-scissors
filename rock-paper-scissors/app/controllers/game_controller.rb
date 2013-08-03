df2b6cb9feffd67c9b537a1632c29050aff6010a
jdlehman <jonathan.lehman91@gmail.com>
2013-08-03 12:51:36 -0400

    Working game logic
    Add game logic to controller.
    Display informative and unstyled views (displays who won and with what).

--- rock-paper-scissors/app/controllers/game_controller.rb
+++ rock-paper-scissors/app/controllers/game_controller.rb
@@ -1,10 +1,51 @@
 class GameController < ApplicationController
-
+	
 	def new
 	end
 
 	def play
+		setup
+		@winner = outcome
+	end
 
+	private
+
+	def setup
+		# generate random computer move
+		# convert to symbol
+		srand Time.new.sec
+		@computer_move = to_symbol(rand(2) + 1)
+		
+		# convert player move to symbole
+		@player_move = to_symbol(params[:move].to_i)
+		p @computer_move
+	end
+
+	def to_symbol(num)
+		case num
+			when 1
+				:rock
+			when 2
+				:paper
+			else
+				:scissors
+		end
+	end
+
+	def outcome
+		# return 1 for player win
+		# -1 for computer win
+		# 0 for tie
+		unless @player_move == @computer_move
+			win = { :rock => :scissors, :paper => :rock, :scissors => :paper }
+			if win[@player_move] == @computer_move
+				1
+			else
+				-1
+			end	
+		else
+			0
+		end
 	end
 
 end
--- rock-paper-scissors/config/routes.rb
+++ rock-paper-scissors/config/routes.rb
@@ -1,4 +1,5 @@
 RockPaperScissors::Application.routes.draw do
 	root :to => 'game#new'
 	post '/throw' => 'game#play'
+	get '/throw' => redirect('/')
 end
